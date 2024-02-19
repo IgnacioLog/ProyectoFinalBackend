4f106b62dcea9473f862b7e7cbe918e146cd1fcc
IgnacioLog <103467993+IgnacioLog@users.noreply.github.com>
2024-02-19 11:05:32 -0300

    procesarpagos2

--- ProyectoFinalBackend/frontend/proyecto-final-frontend/src/App.js
+++ ProyectoFinalBackend/frontend/proyecto-final-frontend/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
 import './App.css';
+import PaymentForm from '../components/PaymentForm.js'; // Asegúrate de que la ruta de importación sea correcta
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Pago con Stripe</h1>
+        <PaymentForm />
       </header>
     </div>
   );
---
+++ ProyectoFinalBackend/frontend/proyecto-final-frontend/src/components/paymentForm.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements, CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const cardElement = elements.getElement(CardElement);
+
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
+      type: 'card',
+      card: cardElement,
+    });
+
+    if (error) {
+      console.log('[error]', error);
+    } else {
+      console.log('[PaymentMethod]', paymentMethod);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement />
+      <button type="submit" disabled={!stripe}>
+        Pagar
+      </button>
+    </form>
+  );
+};
+
+const PaymentForm = () => (
+  <Elements stripe={stripePromise}>
+    <CheckoutForm />
+  </Elements>
+);
+
+export default PaymentForm;
